6841eee1e8fb66ca7f4d19e640c8c89d61993216
unknown <anuj.k.sharma@hotmail.com>
2022-01-24 21:43:51 +0530

    Just to push

---
+++ magician.py
@@ -0,0 +1,18 @@
+magicians = ['alice','david','carolina','shahrukh']
+#for loop is used to repeat the lines
+for magician in magicians:
+    print(magician.title()+ ",that was a great show!")
+
+
+riders = ['Tom cruise','Diesel','The rock','johnson']
+for rider in riders:
+    print(rider.title()+", you did a great job")
+    print("when can I see your next show, " +rider.title() +".\n")
+print("Thank you all magicians and actors as a rider for performing and entertaining us")
+
+
+pizza_variety = ['plain','pan','Mozerilla']
+for pizza_variety in pizza_variety:
+    print(pizza_variety.title()+", I love cause it is plain and no fat")
+    print(pizza_variety.title()+", I do not like because it is so, sticky")
+    print(pizza_variety.title()+",oh I love it")
